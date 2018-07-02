8dc5b5d4f451b7af758f8603fdee07a89ca60f57
Throwback74 <corey.slade@gmail.com>
2018-07-01 23:38:49 -0700

    partial eventful API code added, incomplete

--- assets/javascript/ytube.js
+++ assets/javascript/ytube.js
@@ -82,3 +82,49 @@ var searchArtist = function(artist) {
 };
 
 searchArtist(artist);
+
+
+function show_alert()
+
+{
+
+var oArgs = {
+
+        app_key:"3bGpBSbV8WrbjZcS",
+
+        id: "20218701",
+
+        page_size: 25 ,
+
+};
+
+EVDB.API.call("/events/get", oArgs, function(oData) {
+
+    // Note: this relies on the custom toString() methods below
+
+});
+
+}
+
+var oArgs = {
+
+    app_key: "YOUR APP KEY",
+
+    q: "music",
+
+    where: "San Diego", 
+
+    "date": "2013061000-2015062000",
+
+    page_size: 5,
+
+    sort_order: "popularity",
+
+};
+
+EVDB.API.call("/events/search", oArgs, function(oData) {
+
+    // Note: this relies on the custom toString() methods below
+
+});
+
