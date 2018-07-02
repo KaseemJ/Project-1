74e77dea6cce08143a5c0b4974244d127f2bdcf4
Throwback74 <corey.slade@gmail.com>
2018-07-02 12:19:53 -0700

    changing branches

---
+++ assets/javascript/seatgeek.js
@@ -0,0 +1,18 @@
+var artist = "";
+
+var searchVenue = function(artist) {
+    artist = "The Creepshow";
+
+    var queryURL = 'https://api.seatgeek.com/2/events?geoip=true?client_id=MTIxMzMwNDJ8MTUzMDU1NjA1OS4wNA';
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+
+        // Printing the entire object to console
+        console.log(response);
+        var results = response.items;
+    });
+};
+
+searchVenue(artist);
